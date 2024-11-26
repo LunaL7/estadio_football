f09621b86c533c02441f383809f9a6131a17a855
alving342 <alvin.gutierrez@upr.edu>
2024-11-25 20:32:54 -0400

    added comentarios

--- Cliente.java
+++ Cliente.java
@@ -3,16 +3,17 @@ public class Cliente {
     String correo;
     String telefono;
 
+    //Constructor de Cliente. Tiene parametros para nombre, correo, y telefono
     public Cliente(String nombre, String correo, String telefono){
         this.nombre = nombre;
         this.correo = correo;
         this.telefono = telefono;
     }
 
+    //getters
     public String getNombre(){
         return nombre;
     }
-
     public String getCorreo(){
         return correo;
     }
